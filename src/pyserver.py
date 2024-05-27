2f0f84ef2867ab57c657e645386d1c05342a9abc
Shi-Sheng Yang <fourcolor4c@gmail.com>
2024-05-27 20:37:56 +0800

    update pyserver

--- src/pyserver.py
+++ src/pyserver.py
@@ -6,12 +6,14 @@ from torchvision.utils import save_image
 from PIL import Image  
 import os
 def clip_api(path):
+    class_names = ["airliner", "sports_car", "hummingbird", "Egyptian_cat", "Ox"
+                "golden_retriever", "tailed_frog", "zebra", "container_ship", "trailer_truck"
+                ]
     device = "cpu"
     model, preprocess = clip.load('ViT-B/32', device, download_root="/mysqludf/cache/clip")
-    cifar100 = CIFAR100(root=os.path.expanduser("/mysqludf/cache"), download=True)
     image = Image.open(path)
     image_input = preprocess(image).unsqueeze(0).to(device)
-    text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in cifar100.classes]).to(device)
+    text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in class_names]).to(device)
     # Calculate features
     with torch.no_grad():
         image_features = model.encode_image(image_input)
@@ -24,7 +26,7 @@ def clip_api(path):
     values, indices = similarity[0].topk(5)
 
     # Print the result
-    return indices[0].item()
+    return str(indices[0].item() + 1) + '/' + str(indices[1].item() + 1) + '/' + str(indices[2].item() + 1)
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
